49fc509c75e68293d2c9520c3ef2ec1ed596dae3
Nicolas Romano <nicoezequielromano98@gmail.com>
2022-11-06 13:08:55 -0300

    actualizando

--- index.js
+++ index.js
@@ -1,106 +1,143 @@
-let input = document.getElementById("input");
-let botonInput = document.getElementById("botonInput");
-let div = document.getElementById("div");
+const input = document.getElementById("input");
+const botonInput = document.getElementById("botonInput");
+const div = document.getElementById("div");
 const contenedorCarrito = document.getElementById("contenedorCarrito");
+const formulario = document.getElementById("formulario");
+const usuariolog = document.getElementById("usuario");
+const formularioLog = document.getElementById("formularioLog");
+const botonCarrito = document.getElementById("carrito");
 
 let carrito = [];
 
-/* productos.forEach((producto) => {
-  let productoRenderizado = document.createElement("div");
-  productoRenderizado.innerHTML = `
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="${producto.imagen}" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">${producto.nombre}</h5>
-                <p class="card-text">$ ${producto.precio}</p>
-            </div>
-        <button id=${producto.id}>Comprar</button>
-  </div>
-  `;
-  div.append(productoRenderizado);
-  let boton = document.getElementById(producto.id)
-  boton.addEventListener("click", () => comprarProducto(producto))
-}); */
+let usuarios = [{ usuario: "Nicolas", contraseña: 123456 }];
 
 fetch("./productos.json")
-.then(response=> response.json())
-.then(data =>{
-  data.forEach(producto => {
-    let productoRenderizado = document.createElement("div")
-    productoRenderizado.innerHTML = `
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((producto) => {
+      const { nombre, precio, img, id } = producto;
+      let productoRenderizado = document.createElement("div");
+      productoRenderizado.innerHTML = `
     <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="${producto.img}" alt="Card image cap">
+        <img class="card-img-top" src="${img}" alt="Card image cap">
             <div class="card-body">
-                <h5 class="card-title">${producto.nombre}</h5>
-                <p class="card-text">$ ${producto.precio}</p>
+                <h5 class="card-title">${nombre}</h5>
+                <p class="card-text">$ ${precio}</p>
             </div>
-        <button id=${producto.id}>Comprar</button>
+        <button id=${id}>Comprar</button>
   </div>
   `;
-  div.append(productoRenderizado);
-  let boton = document.getElementById(producto.id)
-  boton.addEventListener("click", () => comprarProducto(producto))
-  })
-})
+      div.append(productoRenderizado);
+      let boton = document.getElementById(producto.id);
+      boton.addEventListener("click", (e) => comprarProducto(producto, e));
+    });
+  });
 
-
-
-const comprarProducto = (producto) =>{
-    let productoExiste = carrito.find(item => item.id===producto.id)
-     if(productoExiste === undefined){
-         carrito.push({
-             id: producto.id,
-             nombre: producto.nombre,
-             precio: producto.precio,
-             imagen: producto.precio,
-             cantidad: 1
-         })
-         actualizarCarrito();
-         Swal.fire({
-          title: 'Agregado al carrito',
-          icon: 'success',
-          text:`${producto.nombre} fue agregado al carrito`,
-          showConfirmButton: false,
-          timer: 1000,
-         })
-     }else{
-         productoExiste.precio = productoExiste.precio + producto.precio,
-         productoExiste.cantidad = productoExiste.cantidad + 1;
-         actualizarCarrito();
-         Swal.fire({
-          title: 'Agregado al carrito',
-          icon: 'success',
-          text:`${producto.nombre} fue agregado al carrito`,
-          showConfirmButton: false,
-          timer: 1000,
-         })
-     } 
- }
-
- const buscarProducto = (string) =>{
-    console.log(string)
-    let productoBuscado = productos.find(producto => producto.nombre.includes(string))
-    console.log(productoBuscado);
-    input.value = ""
-   }
-
-
-   const actualizarCarrito = () => {
-    contenedorCarrito.innerHTML = "";
-
-   carrito.forEach((product) => {
-    const div = document.createElement("div")
-    div.innerHTML = `
-    <p>${product.nombre}</p>
-    <p>Precio:${product.precio}</p>
-    <p>Cantidad: <span>${product.cantidad}</span> </p>
-    `
-    contenedorCarrito.appendChild(div);
-
-   })
+const comprarProducto = (producto) => {
+  let productoExiste = carrito.find((item) => item.id === producto.id);
+  if (productoExiste !== undefined) {
+    (productoExiste.precio = productoExiste.precio + producto.precio),
+      (productoExiste.cantidad = productoExiste.cantidad + 1);
+    actualizarCarrito();
+    Toastify({
+        text: "Agregado al carrito!",
+        className: "info",
+        duration:1500,
+        style: {
+          background: "linear-gradient(to right, #0087FE, #9EB6CC)",
+        }
+      }).showToast();
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  } else {
+    carrito.push({
+      ...producto,
+      cantidad: 1,
+    });
+    actualizarCarrito();
+    Toastify({
+        text: "Agregado al carrito!",
+        className: "info",
+        duration:1500,
+        style: {
+          background: "linear-gradient(to right, #0087FE, #9EB6CC)",
+        }
+      }).showToast();
+    localStorage.setItem("carrito", JSON.stringify(carrito));
   }
+};
 
+const buscarProducto = (string) => {
+  console.log(string);
+  let productoBuscado = "./productos.json".find((producto) =>
+    producto.nombre.includes(string)
+  );
+  console.log(productoBuscado);
+  input.value = "";
+};
 
+const actualizarCarrito = () => {
+  contenedorCarrito.innerHTML = "";
+} 
 
+const eliminarProducto = (producto) => {
+    let productoABorrar = carrito.find((item) => item.id === producto.id);
+    productoABorrar.precio = productoABorrar.precio - producto.precio,
+    productoABorrar.cantidad = productoABorrar.cantidad - 1;
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+  
+  
+const mostrarCarrito = () => {
+      contenedorCarrito.classList.toggle("carritoOn")
+      contenedorCarrito.innerHTML=""
+      carrito.forEach((product) => {
+          const { img, precio, nombre, cantidad, id } = product;
+          const div = document.createElement("div");
+          div.innerHTML = `
+          <div class="card" style="width: 10rem;">
+          <img class="card-img-top" src="${img}" alt="Card image cap">
+          <div class="card-body">
+          <h5 class="card-title">${nombre}</h5>
+          <h5 class="card-title">${cantidad}</h5>
+          <p class="card-text">$ ${precio}</p>
+          </div>
+          <button id=${id}>Eliminar</button>
+          </div>
+          `;
+          contenedorCarrito.appendChild(div);
+          let boton = document.getElementById(product.id);
+          boton.addEventListener("click", () => eliminarProducto(product));
+        });
+    };
+    botonCarrito.addEventListener("click", mostrarCarrito);
+    
+    
+    const guardarEmail = (e) => {
+        e.preventDefault();
+        let usuario = e.target.children[0].value;
+        let pass = Number(e.target.children[1].value);
+        let userExiste = usuarios.find((user) => user.usuario === usuario);
+        if (userExiste.contraseña !== pass) {
+            console.log("contraseña incorrecta");
+        } else if (userExiste.contraseña === pass) {
+            console.log("contraseña correcta. usuario correcto. Bienvenido");
+            localStorage.setItem("usuario", JSON.stringify(usuarios));
+            console.log(usuarioRegistrado);
+        }
+    };
+    
+    let usuarioRegistrado = JSON.parse(localStorage.getItem("usuario"));
+    
+    const bienvenido = (usuario) => {
+        formularioLog.classList.toggle("formLogOn");
+        usuariolog.classList.toggle("usuarioOn");
+        let usuarioRegistrado = document.createElement("div");
+        usuarioRegistrado.innerHTML = `
+    <H1>Bienvenido ${usuario[0].usuario}</H1>`;
+    usuariolog.append(usuarioRegistrado);
+};
 
-botonInput.addEventListener("click", ()=> buscarProducto(input.value));
+usuarioRegistrado != null ? bienvenido(usuarioRegistrado) : false;
+
+formulario.addEventListener("submit", (e) => guardarEmail(e));
+botonInput.addEventListener("click", () => buscarProducto(input.value));
